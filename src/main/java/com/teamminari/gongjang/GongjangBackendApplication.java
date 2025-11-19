4a8a9afcbcad371c63019cf5ec2e76d102490f38
OhJunTaekk <mt3026super@naver.com>
2025-11-19 20:03:56 +0900

    initial commit

---
+++ src/main/java/com/teamminari/gongjang/GongjangBackendApplication.java
@@ -0,0 +1,13 @@
+package com.teamminari.gongjang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongjangBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongjangBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamminari/gongjang/GongjangBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamminari.gongjang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongjangBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
